cb28fd26655cffc8bd7801b41062fa967b113037
Peter Jurczyński <pjurczynski@gmail.com>
2012-08-28 23:31:34 +0200

    diverse production paperclip and dev

--- app/models/main_menu.rb
+++ app/models/main_menu.rb
@@ -1,4 +1,4 @@
 class MainMenu < ActiveRecord::Base
   attr_accessible :desc, :position, :title, :icon
-  has_attached_file :icon, :styles => { :normal => '300x300' }
+  has_attached_file :icon, :styles => { :normal => '300x300' }.merge(PAPERCLIP_STORAGE_OPTIONS)
 end
